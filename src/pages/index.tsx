96c0c2cfdf89dc6a75fc48ee7215f9bde914244d
Gleb Myznikov <myznikovgleb@yandex.ru>
2023-08-02 13:32:18 +0300

    Replace `FancyBox` with `Dummy` on canvas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
 import { Canvas } from '@react-three/fiber'
+import { Environment } from '@react-three/drei'
 
-import FancyBox from '../components/FancyBox'
+import Dummy from '../components/Dummy'
 
 export default function Home() {
   return (
     <div id="canvas">
-      <Canvas>
+      <Canvas shadows camera={{ position: [0, 3, 4] }}>
         <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-        <FancyBox position={[0, 0, 0]} />
+        <directionalLight
+          position={[-2, 2, 2]}
+          castShadow
+          shadow-mapSize={1024}
+        />
+
+        <Dummy position={[0, -1.5, 0]} scale={1.5} />
+
+        <mesh rotation={[-Math.PI / 2, 0, 0]}>
+          <planeGeometry args={[10, 10, 1, 1]} />
+          <shadowMaterial transparent opacity={0.2} />
+        </mesh>
+
+        <Environment preset="city" />
       </Canvas>
     </div>
   )
