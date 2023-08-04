4964061e52afad2be2a7a41c561f0f5ef83093a4
Gleb Myznikov <myznikovgleb@yandex.ru>
2023-08-04 16:37:57 +0300

    Set light daisy ui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {}
   },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: '#ffcce6',
+          'primary-focus': '#ffb3da',
+          accent: '#1dcdbc',
+          neutral: '#2b3440',
+          'base-100': '#ffffff',
+          info: '#3abff8',
+          success: '#36d399',
+          warning: '#fbbd23',
+          error: '#f87272'
+        }
+      }
+    ]
+  },
   plugins: [require('daisyui')]
 }
