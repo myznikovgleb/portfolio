01fcdbcbf143e42c2f8edd36911f22449c367730
Gleb Myznikov <myznikovgleb@yandex.ru>
2023-08-16 19:41:42 +0300

    Update assets

---
+++ src/icons/ArrowRight.tsx
@@ -0,0 +1,19 @@
+interface IconProps {
+  height: number
+  width: number
+}
+
+export function ArrowRight(props: IconProps) {
+  const { height, width } = props
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height={height}
+      viewBox="0 -960 960 960"
+      width={width}
+      fill="white"
+    >
+      <path d="M645-433H135v-94h510L413-759l67-67 346 346-346 345-67-66 232-232Z" />
+    </svg>
+  )
+}
