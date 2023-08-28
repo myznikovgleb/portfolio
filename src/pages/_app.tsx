4f82aea7bc56747de1fe2803a44b47911874903f
Gleb Myznikov <myznikovgleb@yandex.ru>
2023-08-28 23:33:44 +0300

    Add `Layout`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
-import { AppProps } from 'next/app'
+import type { AppProps } from 'next/app'
 
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import dynamic from 'next/dynamic'
 
+const Layout = dynamic(() => import('../components/Layout'), { ssr: false })
 const Sandbox = dynamic(() => import('../components/Sandbox'), {
   ssr: false
 })
 
 export default function Home() {
-  return <Sandbox />
+  return (
+    <Layout>
+      <Sandbox />
+    </Layout>
+  )
 }
