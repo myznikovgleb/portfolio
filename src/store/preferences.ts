1683e0ceddb4847cde0ec581dbf3595106ed3cb0
Gleb Myznikov <myznikovgleb@yandex.ru>
2023-08-30 16:34:10 +0300

    Update `preferences` store

--- src/store/preferences.ts
+++ src/store/preferences.ts
@@ -3,18 +3,32 @@ import { persist } from 'zustand/middleware'
 
 interface Preferences {
   isDarkMode: boolean
+  activeTabIndex: number
+  activeModeIndex: number
 
   setIsDarkMode: (isDarkMode: boolean) => void
+  setActiveTabIndex: (newActiveTabIndex: number) => void
+  setActiveModeIndex: (newActiveModeIndex: number) => void
 }
 
 const usePreferences = create<Preferences>()(
   persist(
     (set) => ({
       isDarkMode: false,
+      activeTabIndex: 1,
+      activeModeIndex: 0,
 
-      setIsDarkMode: (isDarkMode: boolean) =>
+      setIsDarkMode: (isDarkMode) =>
         set(() => ({
           isDarkMode: isDarkMode
+        })),
+      setActiveTabIndex: (newActiveTabIndex) =>
+        set(() => ({
+          activeTabIndex: newActiveTabIndex
+        })),
+      setActiveModeIndex: (newActiveModeIndex) =>
+        set(() => ({
+          activeModeIndex: newActiveModeIndex
         }))
     }),
     { name: 'preferences' }
